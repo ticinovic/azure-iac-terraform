7d3bf4d3f6f213f3c7b933b86b6d708900248764
MarioT <marioticinovic98@gmail.com>
2025-09-04 11:28:46 +0000

    Fix: remove duplicate azurerm_client_config from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,5 @@
 # terraform/main.tf
 
-data "azurerm_client_config" "current" {}
-
 resource "azurerm_resource_group" "main" {
   name     = "rg-${var.project_name}-${var.environment}"
   location = var.location
