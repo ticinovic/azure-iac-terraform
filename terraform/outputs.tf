6e7042b7a697ab985f0d681a49b1cd29b9943ddf
MarioT <marioticinovic98@gmail.com>
2025-09-09 13:01:19 +0000

    outputfs.tf duplicate line

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,7 +7,3 @@ output "web_app_hostname" {
   description = "The default hostname of the deployed Web App."
   value       = azurerm_linux_web_app.main.default_hostname
 }
-output "web_app_hostname" {
-  description = "Default hostname of the deployed Web App"
-  value       = azurerm_linux_web_app.main.default_hostname
-}
